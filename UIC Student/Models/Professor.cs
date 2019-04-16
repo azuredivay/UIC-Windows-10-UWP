cde914de59e2951cc7341c3a5c2252644354b74f
divay pandey <divay.pandey@outlook.com>
2019-04-15 20:41:13 -0500

    set up professor and room classes, cant use System.Device.Location

---
+++ UIC Student/Models/Professor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIC_Student.Models
+{
+    public class Professor
+    {
+        private readonly int iD;
+        private readonly string name;
+        private readonly string netID;
+        private readonly Room office;
+
+        public Professor(int id, string _name, string _netid, Room _office)
+        {
+            iD = id;
+            name = _name;
+            netID = _netid;
+            office = _office;
+        }
+
+        public Room GetOffice() => office;
+        public string GetName() => name;
+        public string GetNetID() => netID;
+        public int GetID() => iD;
+        public bool IsInOffice()
+        {
+            return false; //hold schedule info here?
+        }
+    }
+}
---
+++ UIC Student/Models/Room.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+//using System.Device.Location; //not in UWP?
+namespace UIC_Student.Models
+{
+    public class Room
+    {
+        private readonly string roomCode;
+        private readonly Floor floor;
+        private readonly Building building;
+        private readonly string type; //change to enum?
+        private readonly Geocoordinate location;
+
+        public Room(string _roomcode, Floor _floor, Building _building, string _type, Geocoordinate _latlon)
+        {
+            roomCode = _roomcode;
+            floor = _floor;
+            building = _building;
+            type = _type;
+            location = _latlon;
+        }
+
+        public Tuple<Floor, Building> GetFloorNBuilding() => new Tuple<Floor, Building>(floor, building);
+
+        public Geocoordinate GetLocation() => location;
+
+        public string GetRoomType() => type;
+
+        //public double GetDistanceTo(Geocoordinate loc) => location.GetDistanceTo(loc); //get distance unavable in W.D.G
+    }
+}
